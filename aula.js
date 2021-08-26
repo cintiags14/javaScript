dbf3604ab516371ee49caf59e1556bb9c327853a
cintiags14 <34324451+cintiags14@users.noreply.github.com>
2021-08-26 17:56:27 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,37 @@
+console.log("Cíntia")
+console.log('teste 989898')
+
+let idade = 23;
+console.log(idade)
+
+//let quando pode alterar o valor e const quando o valor é constante
+
+let pessoa = { //objeto
+    nome: "Cíntia",
+    idade: '23',
+    estado: true,
+    telefone: '9898989898',
+    sobrenome: 'Galvao'
+};
+
+console.log(pessoa)
+
+//Arrays
+
+let familia = [23,45,89];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
